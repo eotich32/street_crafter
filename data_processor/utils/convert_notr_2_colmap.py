e8b3989dc3f26abec01095e584c1e73712dcb5bb
anchun <nkterry@163.com>
2025-08-14 16:35:14 +0800

    add mask support

--- data_processor/utils/convert_notr_2_colmap.py
+++ data_processor/utils/convert_notr_2_colmap.py
@@ -36,6 +36,10 @@ if __name__ == '__main__':
     for cam_name in camera_names:
         if not os.path.exists(os.path.join(output_img_path, cam_name)):
             os.makedirs(os.path.join(output_img_path, cam_name))
+    output_masks_path = os.path.join(output_dir, 'masks')
+    for cam_name in camera_names:
+        if not os.path.exists(os.path.join(output_masks_path, cam_name)):
+            os.makedirs(os.path.join(output_masks_path, cam_name))
 
     ego_pose_dir = os.path.join(input_notr_dir, 'ego_pose')
     extrinsics_dir = os.path.join(input_notr_dir, 'extrinsics')
@@ -73,6 +77,9 @@ if __name__ == '__main__':
                     image = cv2.imread(source_img_path)
                     heights[camera_id], widths[camera_id] = image.shape[0], image.shape[1]
                 shutil.copyfile(source_img_path, dest_img_path)
+                source_mask_path = os.path.join(input_notr_dir, 'dynamic_mask', frame + '_' + camera_id + '.jpg')
+                dest_mask_path = os.path.join(output_masks_path, camera_names[int(camera_id)], frame + '_' + camera_id + '.jpg')
+                shutil.copyfile(source_mask_path, dest_mask_path)
                 f.write('{} {} {} {} {} {} {} {} {} {} \n\n'.format(i,w,x,y,z,pos[0], pos[1], pos[2],camera_id, img_name))
                 i = i+1
 
